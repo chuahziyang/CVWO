da2834ef94b80b6b484b75bf7591cff9cae902d4
chuahziyang <czyang2002@gmail.com>
2023-12-19 20:54:42 +0800

    asdasd

--- src/App.tsx
+++ src/App.tsx
@@ -117,22 +117,23 @@ const deployments: commentOverview[] = [
   },
   // More deployments...
 ];
-const activityItems = [
-  {
-    user: {
-      name: "Michael Foster",
-      imageUrl:
-        "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-    },
-    category: "ios-app",
-    commit: "2d89f0c8",
-    branch: "main",
-    date: "1h",
-    dateTime: "2023-01-23T11:00",
-  },
-  // More items...
-];
+// const activityItems = [
+//   {
+//     user: {
+//       name: "Michael Foster",
+//       imageUrl:
+//         "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+//     },
+//     category: "ios-app",
+//     commit: "2d89f0c8",
+//     branch: "main",
+//     date: "1h",
+//     dateTime: "2023-01-23T11:00",
+//   },
+//   // More items...
+// ];
 
+//@ts-ignore
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
@@ -338,7 +339,7 @@ export default function Example() {
                           )}
                         >
                           <span className="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-gray-700 bg-gray-800 text-[0.625rem] font-medium text-gray-400 group-hover:text-white">
-                            {team.initial}
+                            {team.name.charAt(0)}
                           </span>
                           <span className="truncate">{team.name}</span>
                         </a>
