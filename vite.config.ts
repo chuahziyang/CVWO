3bc5d5dc3083a92cd6ccf87fe68bcc9c841246cc
chuahziyang <czyang2002@gmail.com>
2023-12-19 22:25:44 +0800

    asdasd

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { resolve } from "path";
+import { glob, globSync, globStream, globStreamSync, Glob } from "glob";
 
 const root = resolve(__dirname, "src");
+const outdir = resolve(__dirname, "dist");
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   root,
+  build: {
+    emptyOutDir: true,
+    outDir: outdir,
+    rollupOptions: {
+      input: glob.sync(resolve(__dirname, "src", "*.html")),
+    },
+  },
 });
