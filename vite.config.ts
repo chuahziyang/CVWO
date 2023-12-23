8fb5c42a0f4a8f542fc81613ab0e56ecb1dfc090
czyan <czyang2002@gmail,com>
2023-12-23 23:13:47 +0800

    asd

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { resolve } from "path";
 import { glob, globSync, globStream, globStreamSync, Glob } from "glob";
-import Generouted from "@generouted/react-router/plugin";
+import generouted from "@generouted/react-router/plugin";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), Generouted()],
+  plugins: [react(), generouted()],
   resolve: { alias: { "@": "/src" } },
 });
