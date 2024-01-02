93244cfe7393483a7a2da9e5a34af50ab9155fdf
Chuah Zi Yang <czyang2002@gmail.com>
2024-01-03 00:21:50 +0800

    WORKING????

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_01_132905) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_02_155106) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,6 +33,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_01_132905) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin"
+    t.string "password_digest"
   end
 
   add_foreign_key "posts", "users"
