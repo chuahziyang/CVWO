d23d07a49c26a32136696dff2f4be8e60e69f2e2
Chuah Zi Yang <czyang2002@gmail.com>
2024-01-10 03:49:22 +0800

    axioswithAuth

--- frontend/src/server/axios.ts
+++ frontend/src/server/axios.ts
@@ -1,17 +1,5 @@
 import axiosalias from "axios";
 
-const test =
-  "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJleHAiOjE3MDUxNzc0MDd9.0nrmUHZbOcWwDKCtDumq2bgmvH_gqpSVsVLOkOjqf2c";
-
 export const axios = axiosalias.create({
   baseURL: "http://localhost:3000",
 });
-
-export const axioswithAuth = (header: string) => {
-  return axiosalias.create({
-    baseURL: "http://localhost:3000",
-    headers: {
-      Authorization: "Bearer " + header,
-    },
-  });
-};
