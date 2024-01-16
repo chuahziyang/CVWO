b6347f913e318ad12e126198e958b8a9c128e8e7
Chuah Zi Yang <czyang2002@gmail.com>
2024-01-17 05:23:36 +0800

    asd

--- frontend/src/components/postarea.tsx
+++ frontend/src/components/postarea.tsx
@@ -81,6 +81,7 @@ export default function Example({
             <div className="ml-auto pl-3">
               <div className="-mx-1.5 -my-1.5">
                 <button
+                  onClick={() => setiserror(false)}
                   type="button"
                   className="inline-flex rounded-md bg-red-50 p-1.5 text-red-500 hover:bg-red-100 focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-offset-2 focus:ring-offset-red-50"
                 >
