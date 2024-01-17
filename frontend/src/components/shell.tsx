f8862947ff06e4d823f2be2a2e68685a0c6124c3
Chuah Zi Yang <czyang2002@gmail.com>
2024-01-18 05:35:35 +0800

    asda#

--- frontend/src/components/shell.tsx
+++ frontend/src/components/shell.tsx
@@ -168,7 +168,7 @@ function newFunction(background: boolean) {
                         )}
                       >
                         <span className="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-gray-700 bg-gray-800 text-[0.625rem] font-medium text-gray-400 group-hover:text-white">
-                          {post.name.charAt(0)}
+                          {post.name.charAt(0).toUpperCase()}
                         </span>
                         <span className="truncate">{post.name}</span>
                       </a>
