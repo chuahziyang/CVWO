f7472da4f55157184c965e6ee8cd71e4a26542ef
Chuah Zi Yang <czyang2002@gmail.com>
2024-01-18 04:36:39 +0800

    asda

--- frontend/src/components/postarea.tsx
+++ frontend/src/components/postarea.tsx
@@ -133,21 +133,7 @@ export default function Example({
           <nav className="hidden sm:flex" aria-label="Breadcrumb">
             <ol role="list" className="flex items-center space-x-4">
               <li>
-                <div className="flex">
-                  <a
-                    href="/"
-                    className="text-sm font-medium text-gray-400 hover:text-gray-200"
-                  >
-                    Posts
-                  </a>
-                </div>
-              </li>
-              <li>
                 <div className="flex items-center">
-                  <ChevronRightIcon
-                    className="h-5 w-5 flex-shrink-0 text-gray-500"
-                    aria-hidden="true"
-                  />
                   <a
                     href="/"
                     className="ml-4 text-sm font-medium text-gray-400 hover:text-gray-200"
