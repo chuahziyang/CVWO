f0f0e034fc79ceedc2c9a9d415c1ebf3c03f8b7e
Chuah Zi Yang <czyang2002@gmail.com>
2024-01-22 20:59:31 +0800

    asdad

--- frontend/src/server/axios.ts
+++ frontend/src/server/axios.ts
@@ -1,5 +1,10 @@
 import axiosalias from "axios";
 
+const baseURL =
+  process.env.NODE_ENV === "production"
+    ? "https://offbeat-salt-production.up.railway.app"
+    : "http://localhost:3000";
+
 export const axios = axiosalias.create({
-  baseURL: "http://localhost:3000",
+  baseURL: baseURL,
 });
