7deb0e242c82df8e43b82fef2d99ee083ec66d69
Chuah Zi Yang <czyang2002@gmail.com>
2024-01-25 21:27:26 +0800

    asd

--- frontend/src/components/comments.tsx
+++ frontend/src/components/comments.tsx
@@ -125,75 +125,77 @@ const commentBlock = (comments: Comment[]) => {
                   <time>{moment(comment.created_at).fromNow()}</time>
                 </p>
               </div>
-              <Menu as="div" className="relative inline-block text-left">
-                <div>
-                  <Menu.Button className="-m-2 flex items-center rounded-full p-2 text-gray-400 hover:text-gray-600">
-                    <span className="sr-only">Open options</span>
-                    <EllipsisHorizontalIcon
-                      className="h-5 w-5"
-                      aria-hidden="true"
-                    />
-                  </Menu.Button>
-                </div>
-                <Transition
-                  as={Fragment}
-                  enter="transition ease-out duration-100"
-                  enterFrom="transform opacity-0 scale-95"
-                  enterTo="transform opacity-100 scale-100"
-                  leave="transition ease-in duration-75"
-                  leaveFrom="transform opacity-100 scale-100"
-                  leaveTo="transform opacity-0 scale-95"
-                >
-                  <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                    <div className="py-1">
-                      <Menu.Item>
-                        {({ active }) => (
-                          <button
-                            onClick={deleteComment(comment.id)}
-                            className={classNames(
-                              active
-                                ? "bg-gray-100 text-gray-900"
-                                : "text-gray-700",
-                              "flex px-4 py-2 text-sm"
-                            )}
-                          >
-                            <TrashIcon
-                              className="mr-3 h-5 w-5 text-gray-400"
-                              aria-hidden="true"
-                            />
-                            <span>Delete Comment</span>
-                          </button>
-                        )}
-                      </Menu.Item>
-                    </div>
-                    <div className="py-1">
-                      <Menu.Item>
-                        {({ active }) => (
-                          <button
-                            onClick={() => {
-                              setChangecommentOpen(true);
-                              setChangecomment(comment.content);
-                              setCommentid(comment.id);
-                            }}
-                            className={classNames(
-                              active
-                                ? "bg-gray-100 text-gray-900"
-                                : "text-gray-700",
-                              "flex px-4 py-2 text-sm"
-                            )}
-                          >
-                            <PencilIcon
-                              className="mr-3 h-5 w-5 text-gray-400"
-                              aria-hidden="true"
-                            />
-                            <span>Edit Comment</span>
-                          </button>
-                        )}
-                      </Menu.Item>
-                    </div>
-                  </Menu.Items>
-                </Transition>
-              </Menu>
+              {comment.user_id === cookie?.user?.id && (
+                <Menu as="div" className="relative inline-block text-left">
+                  <div>
+                    <Menu.Button className="-m-2 flex items-center rounded-full p-2 text-gray-400 hover:text-gray-600">
+                      <span className="sr-only">Open options</span>
+                      <EllipsisHorizontalIcon
+                        className="h-5 w-5"
+                        aria-hidden="true"
+                      />
+                    </Menu.Button>
+                  </div>
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                  >
+                    <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                      <div className="py-1">
+                        <Menu.Item>
+                          {({ active }) => (
+                            <button
+                              onClick={deleteComment(comment.id)}
+                              className={classNames(
+                                active
+                                  ? "bg-gray-100 text-gray-900"
+                                  : "text-gray-700",
+                                "flex px-4 py-2 text-sm"
+                              )}
+                            >
+                              <TrashIcon
+                                className="mr-3 h-5 w-5 text-gray-400"
+                                aria-hidden="true"
+                              />
+                              <span>Delete Comment</span>
+                            </button>
+                          )}
+                        </Menu.Item>
+                      </div>
+                      <div className="py-1">
+                        <Menu.Item>
+                          {({ active }) => (
+                            <button
+                              onClick={() => {
+                                setChangecommentOpen(true);
+                                setChangecomment(comment.content);
+                                setCommentid(comment.id);
+                              }}
+                              className={classNames(
+                                active
+                                  ? "bg-gray-100 text-gray-900"
+                                  : "text-gray-700",
+                                "flex px-4 py-2 text-sm"
+                              )}
+                            >
+                              <PencilIcon
+                                className="mr-3 h-5 w-5 text-gray-400"
+                                aria-hidden="true"
+                              />
+                              <span>Edit Comment</span>
+                            </button>
+                          )}
+                        </Menu.Item>
+                      </div>
+                    </Menu.Items>
+                  </Transition>
+                </Menu>
+              )}
 
               <div
                 id="dropdownComment3"
